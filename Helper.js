f370cb5f558edc1fe045d9aeda34e7b06396d7e1
Konrad-Jalowy <sitesystem@wp.pl>
2024-11-05 14:01:38 +0100

    Helper cls in sep file

---
+++ Helper.js
@@ -0,0 +1,83 @@
+class Helper {
+    constructor(){
+        this.currOp = document.querySelector(".currOp");
+        this.prevOp = document.querySelector(".prevOp");
+    }
+
+    update(curr, prev="", op=""){
+        this.currOp.textContent = curr;
+        this.prevOp.textContent = `${prev}${op}`;
+    }
+
+    compute(curr, prev, op){
+        // if(curr.endsWith("!")){
+        //     curr = this.computeFactorial(curr);
+        // }
+        let currFloat = parseFloat(curr);
+        let prevFloat = parseFloat(prev);
+        let output;
+        switch(op){
+            case "+":
+                output = prevFloat + currFloat;
+                break;
+            case "-":
+                output = prevFloat - currFloat;
+                break;
+            case "/":
+                if(currFloat !== 0){
+                    output = prevFloat / currFloat;
+                } else {
+                    output = 0;
+                }
+                break;
+            case "*":
+                output = prevFloat * currFloat;
+                break;
+        }
+        return output;
+    }
+
+    factorial(number){
+        let fact = 1;
+        do {fact = fact * number--; } while (number > 1);
+        return fact;
+    }
+
+    computeFactorial(numberAsString){
+        let toNumber = parseFloat(numberAsString);
+        let output = this.factorial(toNumber);
+        return output.toString();
+    }
+
+    computePow2(numberAsString){
+        let toNumber = parseFloat(numberAsString);
+        let output = Math.pow(toNumber, 2);
+        return output.toString();
+    }
+
+    computeSqrt(numberAsString){
+        let toNumber = parseFloat(numberAsString);
+        let output = Math.sqrt(toNumber);
+        return output.toString();
+    }
+
+    animateWrongInput(){
+        document.querySelector(".output").animate(
+            [
+              {border: `2px solid red`},
+              {transform: 'translate(1px, 1px)', border: `2px solid red`},
+              {transform: 'translate(-2px, -2px)', border: `1px solid red`},
+              {transform: 'translate(2px, 2px)'},
+              {transform: 'translate(-2px, 3px)'},
+              {transform: 'translate(2px, -3px)'},
+              
+            ],
+            {
+              duration: 400,
+              iterations: 1,
+            },
+          );
+    }
+}
+
+export default Helper;
